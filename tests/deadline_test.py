dd884f5060ae5282dd8eafea6bab6c531d580de1
BT <berekettekle0397@gmail.com>
2026-02-25 21:32:49 +0300

    remove test from tracking

--- tests/deadline_test.py
+++
@@ -1,4 +0,0 @@
-from models.task import Task
-tasks = {
-    "A": Task("A", "Task A", "User1", 8, 2, 10),
-}
--- tests/time_test.py
+++
@@ -1,6 +0,0 @@
-from models.task import Task
-tasks ={
-     "A": Task("A", "Task A", "User1", 9, 2, 12),
-     "B": Task("B", "Task B", "User1", 10, 2, 14),
-}
-    
